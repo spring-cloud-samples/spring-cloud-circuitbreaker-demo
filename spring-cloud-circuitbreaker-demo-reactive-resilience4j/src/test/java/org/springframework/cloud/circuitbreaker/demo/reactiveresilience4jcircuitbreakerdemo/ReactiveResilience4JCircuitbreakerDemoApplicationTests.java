4c3448a42528452846d0adce80e396e6113e0597
Ryan Baxter <rbaxter@vmware.com>
2022-01-27 12:16:35 -0500

    Update to 2020.0.x release

--- spring-cloud-circuitbreaker-demo-reactive-resilience4j/src/test/java/org/springframework/cloud/circuitbreaker/demo/reactiveresilience4jcircuitbreakerdemo/ReactiveResilience4JCircuitbreakerDemoApplicationTests.java
+++ spring-cloud-circuitbreaker-demo-reactive-resilience4j/src/test/java/org/springframework/cloud/circuitbreaker/demo/reactiveresilience4jcircuitbreakerdemo/ReactiveResilience4JCircuitbreakerDemoApplicationTests.java
@@ -1,11 +1,10 @@
 package org.springframework.cloud.circuitbreaker.demo.reactiveresilience4jcircuitbreakerdemo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReactiveResilience4JCircuitbreakerDemoApplicationTests {
 
--- spring-cloud-circuitbreaker-demo-resilience4j/src/test/java/org/springframework/cloud/circuitbreaker/demo/resilience4jcircuitbreakerdemo/Resilience4JCircuitbreakerDemoApplicationTests.java
+++ spring-cloud-circuitbreaker-demo-resilience4j/src/test/java/org/springframework/cloud/circuitbreaker/demo/resilience4jcircuitbreakerdemo/Resilience4JCircuitbreakerDemoApplicationTests.java
@@ -1,11 +1,9 @@
 package org.springframework.cloud.circuitbreaker.demo.resilience4jcircuitbreakerdemo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class Resilience4JCircuitbreakerDemoApplicationTests {
 
